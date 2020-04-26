21ebeda5b2b76c556fa64ad139273f5970e6a279
Bobtresh <61907749+Bobtresh@users.noreply.github.com>
2020-04-26 13:10:17 +0200

    Add files via upload

---
+++ TransistorPinnen.h
@@ -0,0 +1,28 @@
+//TransistorPinnen.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+//#include "SPI-GPIO.h"
+
+
+typedef struct{
+	int basisGateChannel;
+	int collectorDrainChannel;
+	int emitterSourceChannel;
+	char* type;		//pnp of npn
+	char* structuur; //MOSFET of BJT
+} Transistor;
+
+
+bool chose_config(const char*, int);
+
+bool locate_base(Transistor*);
+
+double measure_current(int);
+
+void zet_potwaarde(int, int);
+
+int stroom_door_pin(int);
+
+void locate_collector_emitter(Transistor*);
