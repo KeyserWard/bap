69bb6b595dc28480bf92cbef3e30d7c0877a22c1
Bobtresh <61907749+Bobtresh@users.noreply.github.com>
2020-04-26 12:05:52 +0200

    Add files via upload

---
+++ TransistorPinnen.c
@@ -0,0 +1,155 @@
+#include "TransistorPinnen.h"
+
+#define R_W 50		//in ohm
+#define R_AB 10000	//in ohm
+typedef struct{
+	int basisGateChannel;
+	int collectorDrainChannel;
+	int emitterSourceChannel;
+	char* type;		//pnp of npn
+	char* structuur; //MOSFET of BJT
+} Transistor;
+
+bool chose_config(char* configuratie, int channel) {	// returnt true als de cofiguratie correct is doorgegeven
+	if(!strcmp(configuratie, "Open")){				
+		set_switch(channel, false);
+		return true;
+	} 
+		
+	else if(!strcmp(configuratie, "Vcc")){
+		set_switch(channel, true);
+		set_dac_voltage(channel, 12);	//Vcc = 12V
+		return true;
+	}
+	
+	else if(!strcmp(configuratie, "Ground")){
+		set_switch(channel, true);
+		set_dac_voltage(channel, 0);		//Ground = 0V
+		return true;
+	} 
+	else{
+		return false;	
+	}
+
+}
+
+int stroom_door_pin(int channel){ //channel van de gevraagde digipot
+	if (get_current(channel) > 0)
+		return 1;
+	else if (get_current(channel) < 0)
+		return -1;
+	else return 0;
+}
+
+bool locate_base(Transistor* trans){
+	const char* const configLijst[3] = {"Open", "Vcc", "Ground"};
+	const int permuLijst1[6] = {1, 2, 0, 2, 0, 1};
+	const int permuLijst2[6] = {2, 1, 2, 0, 1, 0};
+	int metingLijst[6][3];					//{{b0, b1, b2}{b1, e1, c1}...}
+	int i=0, j=0;
+	bool eersteInGevonden = false;
+	bool eersteUitGevonden = false;
+	int inBewaar, uitBewaar;
+
+	//eerst alle mogelijke permutaties uitmeten
+	
+	while(i<6){
+		
+		//configuratie kiezen
+		chose_config(configLijst[j], switches[0].channel);						// 0 0 1 1 2 2 
+		chose_config(configLijst[permuLijst1[i]], switches[1].channel);			// 1 2 0 2 0 1
+		chose_config(configLijst[permuLijst2[i]], switches[2].channel);			// 2 1 2 0 1 0
+		
+		
+		//meten
+		metingLijst[i][0]= stroom_door_pin(switches[0].channel);			
+		metingLijst[i][1] = stroom_door_pin(switches[1].channel);
+		metingLijst[i][2] = stroom_door_pin(switches[2].channel);
+		
+		i++;
+		j = i/2;
+		/*mogelijks een wachtlus nodig zodat de meting niet verstoord wordt
+		int w;
+		for(w=0;w<1000;w++){}
+		*/
+	}
+	//zoeken naar de basis
+		for(i=0;i<6;i++){
+		for(j=0;j<3;j++){
+			if(metingLijst[i][j] == 1){
+				if(!eersteInGevonden){
+					inBewaar = j;
+					eersteInGevonden = true;
+				}
+				else {
+					if(inBewaar == j){
+						transistor->basisGateChannel = switches[j].channel;
+						transistor->type = "NPN";
+						return true;
+					}
+				}	
+			}
+			
+			if(metingLijst[i][j] == -1){
+				if(!eersteUitGevonden){
+					uitBewaar = j;
+					eersteUitGevonden = true;
+				}
+				else{
+					if(uitBewaar == j){
+						transistor->basisGateChannel = switches[j].channel;
+						transistor->type = "PNP";
+						return true;
+					}
+				}
+			}
+		}	
+	}
+	if(transistor->basisGateChannel == -1 || strcmp(transistor->type, "Onbepaald") ){
+		return false;																	//false wanneer de geen basis of type gevonden is, dus transistor is defect
+	}
+
+}
+
+double Weerstand(int channel, int R_WB){
+	double weerstandEff;
+	unsigned char value;
+	value = (R_WB - 3*R_W) * 256/R_AB ;		//STROOM DOOR DE PINNEN MOGEN NIET GROTER DAN 5mA
+											//R_WB =	D/256*R_AB + 3*R_W
+	set_digipot(channel, value);
+	return weerstandEff;
+}
+
+void locate_collector_emitter(Transistor* transistor){
+	int i, basisIndex, emitterIndex, collectorIndex;
+	double meting, weerstand;
+	int channels[3] = {CHANNEL1, CHANNEL2 ,CHANNEL3};
+													
+	for(i=0; i<3; i++){		
+		Weerstand(channels[i], 5000);
+		if(channels[i] == transistor->basisGateChannel)
+			basisIndex = i;
+	}
+	collectorIndex = (basisIndex + 1) %3;
+	emitterIndex = 	(basisIndex + 2) %3;											
+
+	chose_config("Vcc", transistor->basisGate);	
+	chose_config("Vcc", channels[collectorIndex]);		
+	chose_config("Ground", channels[emitterIndex]);		
+	meting = measure_current(channels[emitterIndex]); 			// double measure_current(int pin) dus stroom meten door de pin van de emitter											
+	chose_config("Ground", channels[collectorIndex]);		
+	chose_config("Vcc", channels[emitterIndex]);		
+	
+	if(meting <  measure_current(channels[collectorIndex])){
+		//de 2 GES met elkaar vergelijken
+		transistor->collectorDrain = channels[collectorIndex];		//GES in eerste situatie
+		transistor->emitterSource = channels[emitterIndex];			
+	} else {
+		transistor->collectorDrain = channels[emitterIndex];			//GES in tweede situatie
+		transistor->emitterSource = channels[collectorIndex];
+	}
+	
+}
+}
+
+
