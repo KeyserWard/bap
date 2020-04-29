8c3731100b08a01d563275a4855bde075cbec0b1
Bobtresh <61907749+Bobtresh@users.noreply.github.com>
2020-04-29 16:56:42 +0200

    Update metingenOpnemen.c

--- metingenOpnemen.c
+++ metingenOpnemen.c
@@ -12,24 +12,22 @@
 
 double* meting_IC_VCE(Transistor* trans, int xWaarde, double IB){
 	double* punt = (double*)malloc(sizeof(double)*2);
-	double VCE;
-	int RB;
+	double VCE = xWaarde / 4;		//VCE zal max tot (METINGAANTAL - 1) / 4 gaan => 5V
+	int RB = (int)VCE / IB
 	//VCE instellen afhankelijk van x waarde
-	VCE = xWaarde / 4;
-	Weerstand(trans->collectorDrainChannel, 5000);
-	Weerstand(trans->emitterSourceChannel, 5000); //moet dit?
+	
+	Weerstand(trans->collectorDrainChannel, 2000);	//bij VCE = 5V mag I max = 5mA => 1k 
+	Weerstand(trans->emitterSourceChannel, 2000); //moet dit?
 	set_dac_voltage(trans->collectorDrainChannel, VCE);
 	set_dac_voltage(trans->emitterSourceChannel, 0);
-	/*mogelijke controle?
-	VCE = get_adc_voltage(trans->collectorDrainChannel) - get_adc_voltage(trans->emitterSourceChannel);
-	*/
-	punt[0] = VCE;
+
+
 	//IB instellen
-	RB = 
-	Weerstand(trans->basisGateChannel, (int)RB);
-	set_dac_voltage(trans->basisGateChannel, IB*RB);
+	Weerstand(trans->basisGateChannel, RB);
+	set_dac_voltage(trans->basisGateChannel, VCE);	//VBE = VCE
 	
 	//IC opmeten
+	punt[0] = get_adc_voltage(trans->collectorDrainChannel) - get_adc_voltage(trans->emitterSourceChannel); //VCE controle
 	punt[1] = get_current(trans->collectorDrainChannel);
 	
 	return punt;
@@ -47,13 +45,13 @@ double* meting_beta_IC(Transistor* trans, int xWaarde){
 	return punt;
 }
 
-double** data_graph(Transistor* trans, bool IC, bool beta){ //IC = true, IC tov VCE. IC = False, IB tov VCE. beta = true, beta tov IC
+double** data_graph(Transistor* trans, bool IC, bool beta, double IB){ //IC = true, IC tov VCE. IC = False, IB tov VCE. beta = true, beta tov IC
 	int i;
 	double** coorArr= (double**)malloc(sizeof(double*)*METINGAANTAL);
 	for(i=0;i<METINGAANTAL;i++){
 	//	coorArr[i] = (double*)malloc(sizeof(double)*2);
 		if(IC && !beta){
-			coorArr[i] = meting_IC_VCE(trans, i);
+			coorArr[i] = meting_IC_VCE(trans, i, IB);
 			
 		}
 		else if(!IC && !beta){
@@ -70,7 +68,6 @@ double** data_graph(Transistor* trans, bool IC, bool beta){ //IC = true, IC tov 
 		
 	}
 
-
 	return coorArr;
 	
 }
@@ -82,3 +79,14 @@ void free_data(double** data){
 	free(data);
 }
 
+
+int main(){		//testomgeving
+	Transistor trans = {CHANNEL1, CHANNEL2, CHANNEL3, "npn", "BJT"};
+	bool IC = true;
+	bool beta = false;
+	double IB = 0.005 //5mA
+	double** coorArr = data_graph(&trans, IC, beta, IB);
+	free_data(coorArr);
+	
+	return 0;
+}
