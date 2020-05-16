c86168d0147a86436152302b4e4271f8eea5972d
Maarten De Bosschere <maarten.debosschere@outlook.com>
2020-05-16 15:35:25 +0200

    Aanpassen header

--- metingenTransistor.h
+++ metingenTransistor.h
@@ -8,6 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void meting_IC_VCE(Transistor*, double, double***, int);
+void meting_IC_VCE(Transistor*, double, double*, double*, int);
 
 void meting_beta_IC(Transistor*, double***, int);
