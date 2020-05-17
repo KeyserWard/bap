3d5f6fd72e24da97c62a0efb8cf5241da9c3d920
Maarten De Bosschere <maarten.debosschere@outlook.com>
2020-05-17 18:18:25 +0200

    Pointer vergeten in de header

--- TransistorMetingen.h
+++ TransistorMetingen.h
@@ -11,7 +11,7 @@
 #include <string.h>
 #include <math.h>
 
-double meting_VBE(Transistor, double, int);
+double meting_VBE(Transistor*, double, int);
 
 void meting_IC_VCE(Transistor*, double, double*, double*, int);
 
